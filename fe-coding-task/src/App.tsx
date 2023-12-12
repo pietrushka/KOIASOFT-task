6eea7e7e19f75f12c782504b0f2154f204c8d691
pietrushka <piotrekwisnia18@gmail.com>
2023-12-12 17:41:57 +0100

    feature: compose components

--- fe-coding-task/src/App.tsx
+++ fe-coding-task/src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import QueryForm from "./components/QueryForm"
+import Chart from "./components/Chart"
+import { BrowserRouter } from "react-router-dom"
+import { DataProvider } from "./DataContext"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div>
+				<DataProvider>
+					<QueryForm />
+					<Chart />
+				</DataProvider>
+			</div>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
---
+++ fe-coding-task/src/paramOptions.ts
@@ -0,0 +1,15 @@
+import { MIN_QUARTER_YEAR } from "./constants"
+import { populateQuarterRange, getPreviousYearQuarterString } from "./utils"
+
+export const quarterOptions = populateQuarterRange(
+	MIN_QUARTER_YEAR,
+	getPreviousYearQuarterString()
+).map((value) => ({
+	value,
+}))
+
+export const houseTypeOptions = [
+	{ label: "Boliger i alt", value: "00" },
+	{ label: "Småhus", value: "02" },
+	{ label: "Blokkleiligheter", value: "03" },
+]
